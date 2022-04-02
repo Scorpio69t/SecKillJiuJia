e2a2d969e2668880238685b5b0c4ab7fd5710401
瑞涛 杨 <18728163854@163.com>
2022-04-02 16:54:34 +0800

    查询城市Code

---
+++ service/seckill/cities.go
@@ -0,0 +1,109 @@
+package seckill
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"seckill-jiujia/conf"
+	"seckill-jiujia/pkg/logging"
+
+	"go.uber.org/zap"
+)
+
+type Location struct {
+	Code string `json:"code"`
+	Data []struct {
+		Name  string `json:"name"`
+		Value string `json:"value"`
+	} `json:"data"`
+	Ok bool `json:"ok"`
+}
+
+// GetProvinceCode 获取省份code
+func (s *SecKillService) GetProvinceCode() (string, error) {
+	res := Location{}
+	resp, err := s.Request.Get(conf.Conf.SeckillInfo.AllCitiesUrl, nil, nil)
+	if err != nil {
+		logging.Error("get province code failed", zap.Error(err))
+		return "", err
+	}
+	err = json.Unmarshal(resp, &res)
+	if err != nil {
+		logging.Error("unmarshal response body failed", zap.Error(err), zap.Any("body", resp))
+		return "", err
+	}
+
+	for _, v := range res.Data {
+		if v.Name == conf.Conf.SeckillInfo.Province {
+			return v.Value, nil
+		}
+	}
+	return "", errors.New("province code not found")
+}
+
+// GetCityCode 获取城市code
+func (s *SecKillService) GetCityCode() (string, error) {
+	provinceCode, err := s.GetProvinceCode()
+	if err != nil {
+		logging.Error("get province code failed", zap.Error(err))
+		return "", err
+	}
+
+	res := Location{}
+	resp, err := s.Request.Get(conf.Conf.SeckillInfo.AllCitiesUrl, map[string]string{"parentCode": provinceCode}, nil)
+	if err != nil {
+		logging.Error("get city code failed", zap.Error(err))
+		return "", err
+	}
+
+	err = json.Unmarshal(resp, &res)
+	if err != nil {
+		logging.Error("unmarshal response body failed", zap.Error(err), zap.Any("body", resp))
+		return "", err
+	}
+
+	for _, v := range res.Data {
+		if v.Name == conf.Conf.SeckillInfo.City {
+			return v.Value, nil
+		}
+	}
+	return "", errors.New("city code not found")
+}
+
+// GetAllCitiesCode 获取所有城市code
+func (s *SecKillService) GetAllCitiesCode() (map[string]string, error) {
+	province := Location{}
+	city := Location{}
+	r := make(map[string]string)
+
+	resp, err := s.Request.Get(conf.Conf.SeckillInfo.AllCitiesUrl, nil, nil)
+	if err != nil {
+		logging.Error("get province code failed", zap.Error(err))
+		return nil, err
+	}
+	err = json.Unmarshal(resp, &province)
+	if err != nil {
+		logging.Error("unmarshal response body failed", zap.Error(err), zap.Any("body", resp))
+		return nil, err
+	}
+
+	for _, v := range province.Data {
+		resp2, err := s.Request.Get(conf.Conf.SeckillInfo.AllCitiesUrl, map[string]string{"parentCode": v.Value}, nil)
+		if err != nil {
+			logging.Error("get city code failed", zap.Error(err))
+			return nil, err
+		}
+
+		err = json.Unmarshal(resp2, &city)
+		if err != nil {
+			logging.Error("unmarshal response body failed", zap.Error(err), zap.Any("body", resp2))
+			return nil, err
+		}
+
+		for _, v2 := range city.Data {
+			r[v2.Value] = fmt.Sprintf("%s-%s", v.Name, v2.Name)
+		}
+	}
+
+	return r, nil
+}
--- service/seckill/seckill.go
+++ service/seckill/seckill.go
@@ -2,15 +2,13 @@ package seckill
 
 import (
 	"seckill-jiujia/conf"
-	"seckill-jiujia/service"
 )
 
 type SecKillService struct {
 	Request *Request
-	Name    service.ServiceType `json:"name"`
 }
 
-func (s *SecKillService) New(name service.ServiceType) (service.Service, error) {
+func New() (*SecKillService, error) {
 	headers := make(map[string]string)
 	headers["User-Agent"] = conf.Conf.SeckillInfo.UserAgent
 	headers["tk"] = conf.Conf.SeckillInfo.Tk
@@ -18,6 +16,5 @@ func (s *SecKillService) New(name service.ServiceType) (service.Service, error) 
 	req := NewRequest(conf.Conf.SystemInfo.Verbose, headers)
 	return &SecKillService{
 		Request: req,
-		Name:    name,
 	}, nil
 }
